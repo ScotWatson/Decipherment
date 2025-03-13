2cb8d6290ae40f979abe40467e6bfc91da7423a6
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-13 19:17:48 -0400

    Create app.mjs

---
+++ docs/app.mjs
@@ -0,0 +1,39 @@
+/*
+(c) 2025 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import * as Main from "./main.mjs";
+
+function openFileDialog() {
+  return new Promise((resolve, reject) => {
+    const input = document.createElement("input");
+    input.style.display = "none";
+    input.addEventListener("change", (evt) => {
+      resolve(input.files[0]);
+    });
+    input.click();
+    input.remove();
+  });
+}
+
+const btnOpen = document.createElement("button");
+btnOpen.addEventListener("click", readFile);
+
+(async readFile() => {
+  const file = await openFileDialog();
+  const contents = await file.text();
+  const unigrams = new Map();
+  for (const char of contents) {
+    const record = unigrams.get(char);
+    if (record) {
+      ++record.count;
+    } else {
+      unigrams.set(char, {
+        char,
+        count: 0,
+      })
+    }
+  }
+  console.log(Array.from(unigrams.entries()));
+})();
