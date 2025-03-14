db410d261608bc8f6126aa93dc3591b237556255
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-13 20:17:40 -0400

    Update app.mjs

--- docs/app.mjs
+++ docs/app.mjs
@@ -39,5 +39,42 @@ async function readFile() {
       })
     }
   }
-  console.log(Array.from(unigrams.entries()));
+  for (const unigram of unigrams) {
+    unigram.estimate = unigram.count / contents.length;
+    unigram.variance = unigram.estimate * (1 - unigram.estimate);
+  }
+  const bigrams = new Map();
+  let prevChar = contents[0];
+  for (let i = 1; i < contents.length; ++i) {
+    const bigram = prevChar + char;
+    const record = bigrams.get(bigram);
+    if (record) {
+      ++record.instances.push(i - 1);
+    } else {
+      bigrams.set(bigram, {
+        str: bigram,
+        instances: [],
+      });
+    }
+    prevChar = char;
+  }
+  for (const bigramRecord of bigrams) {
+    const bigramCount = bigramRecord.instances.length;
+    const char0Record = unigrams.get(bigramRecord.str[0]);
+    const char1Record = unigrams.get(bigramRecord.str[1]);
+    const bigramPrefixEstimate = bigramRecord.instances.length / char0Record.count;
+    const bigramPrefixVariance = bigramPrefixEstimate * (1 - bigramPrefixEstimate);
+    const bigramSuffixEstimate = bigramRecord.instances.length / char1Record.count;
+    const bigramSuffixVariance = bigramSuffixEstimate * (1 - bigramSuffixEstimate);
+    const prefixEstimate = bigramPrefixEstimate - char0Record.estimate;
+    const prefixVariance = bigramPrefixVariance + char0Record.variance;
+    const prefixStdev = Math.sqrt(prefixVariance);
+    bigramRecord.prefixZ = prefixEstimate / prefixStdev;
+    const suffixEstimate = bigramSuffixEstimate - char0Record.estimate;
+    const suffixVariance = bigramSuffixVariance + char0Record.variance;
+    const suffixStdev = Math.sqrt(suffixVariance);
+    bigramRecord.suffixZ = suffixEstimate / suffixStdev;
+  }
+  console.log(Array.from(unigrams.entries()).sort((entry1, entry2) => { return (entry1[1].count < entry2[1].count) ? 1 : -1; }));
+  console.log(Array.from(bigrams.entries()).sort((entry1, entry2) => { return (entry1[1].prefixZ < entry2[1].prefixZ) ? 1 : -1; }));
 }
